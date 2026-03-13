31fbe4b22ccd74e8e4de0ac77fec48498ddfda30
Mastan Vali <mastanvalishaikIt@gmail.com>
2026-03-13 23:21:59 +0530

    fixing route

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/React-shopping-cart'>
   <StrictMode>
     <App />
   </StrictMode>,
