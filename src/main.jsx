9b0e659fe619c7ca5d7857c2a134a701edfd1d4b
Mastan Vali <mastanvalishaikIt@gmail.com>
2026-03-13 23:40:04 +0530

    First Project

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/React-shopping-cart'>
+  <BrowserRouter>
   <StrictMode>
     <App />
   </StrictMode>,
